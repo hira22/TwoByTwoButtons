f9bbd08776a6a8d4c4db1798f012dc1064830320
hira22 <masayuki.hiraoka@ietty.co.jp>
2021-12-15 10:18:32 +0900

    add iOS15+

--- TwoByTwoButtons/ContentView.swift
+++ TwoByTwoButtons/ContentView.swift
@@ -14,9 +14,13 @@ struct ContentView: View {
             Section("iOS13+") {
                 IOS13_TwoByTwoButtonsView()
             }
-            
+
             Section("iOS14+") {
                 IOS14_TwoByTwoButtonsView()
+            }
+
+            Section("iOS15+") {
+                IOS15_TwoByTwoButtonsView()
             }
         }
         .padding(.horizontal)
@@ -26,12 +30,12 @@ struct ContentView: View {
 // MARK: ButtonType
 enum ButtonType: CaseIterable, Identifiable {
     var id: Int { self.hashValue }
-    
+
     case a
     case b
     case c
     case d
-    
+
     var text: String {
         switch self {
         case .a:
@@ -44,7 +48,7 @@ enum ButtonType: CaseIterable, Identifiable {
             return "DDDDDDDDDDDDDDD"
         }
     }
-    
+
     var color: Color {
         switch self {
         case .a:
@@ -57,7 +61,7 @@ enum ButtonType: CaseIterable, Identifiable {
             return .cyan
         }
     }
-    
+
     var icon: Image {
         switch self {
         case .a:
@@ -80,39 +84,39 @@ struct IOS13_TwoByTwoButtonsView: View {
                 Button(action: {}) {
                     ButtonContentView(type: .a)
                 }
-                
+
                 Button(action: {}) {
                     ButtonContentView(type: .b)
                 }
             }
-            
+
             HStack {
                 Button(action: {}) {
                     ButtonContentView(type: .c)
                 }
-                
+
                 Button(action: {}) {
                     ButtonContentView(type: .d)
                 }
             }
         }
     }
-    
+
     private struct ButtonContentView: View {
         let type: ButtonType
-        
+
         var body: some View {
             HStack {
                 type.icon
                     .resizable()
                     .frame(width: 20, height: 20)
-                
+
                 Text(type.text)
                     .font(.headline)
                     .minimumScaleFactor(0.9)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .multilineTextAlignment(.leading)
-                
+
             }
             .frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50)
             .padding(.horizontal, 8)
@@ -131,7 +135,7 @@ struct IOS14_TwoByTwoButtonsView: View  {
         .init(.flexible()),
         .init(.flexible()),
     ]
-    
+
     var body: some View {
         LazyVGrid(columns: gridLayout) {
             ForEach(ButtonType.allCases) { type in
@@ -140,13 +144,13 @@ struct IOS14_TwoByTwoButtonsView: View  {
                         type.icon
                             .resizable()
                             .frame(width: 20, height: 20)
-                        
+
                         Text(type.text)
                             .font(.headline)
                             .minimumScaleFactor(0.9)
                             .frame(maxWidth: .infinity, alignment: .leading)
                             .multilineTextAlignment(.leading)
-                        
+
                     }
                     .padding(.horizontal, 8)
                 }
@@ -160,6 +164,40 @@ struct IOS14_TwoByTwoButtonsView: View  {
         }
     }
 }
+
+// MARK: IOS15_TwoByTwoButtonsView
+struct IOS15_TwoByTwoButtonsView: View  {
+    let gridLayout: [GridItem] = [
+        .init(.flexible()),
+        .init(.flexible()),
+    ]
+
+    var body: some View {
+        LazyVGrid(columns: gridLayout) {
+            ForEach(ButtonType.allCases) { type in
+                Button(action: {}) {
+                    HStack {
+                        type.icon
+                            .resizable()
+                            .frame(width: 20, height: 20)
+
+                        Text(type.text)
+                            .font(.headline)
+                            .minimumScaleFactor(0.8)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .multilineTextAlignment(.leading)
+
+                    }
+                    .frame(height: 40)
+                }
+                .tint(type.color)
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle(radius: 4))
+            }
+        }
+    }
+}
+
 
 // MARK: ContentView_Previews
 struct ContentView_Previews: PreviewProvider {
